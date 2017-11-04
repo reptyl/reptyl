4b402326a0b84f16d9e25cdf588a55e412219ccd
Stefano Varesi <stefano.varesi@gmail.com>
2017-11-04 11:11:01 +0100

    removed unused class

--- src/main/java/io/reptyl/UtilityClassInstantiationError.java
+++
@@ -1,8 +0,0 @@
-package io.reptyl;
-
-public class UtilityClassInstantiationError extends AssertionError {
-
-    public UtilityClassInstantiationError() {
-        super("this utility class cannot be instantiated");
-    }
-}
